51d7b9edf9eb69baac6b2492ee46c835fb43140c
Robin <luis.guillenn21@gmail.com>
2025-02-19 16:35:56 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ProyectoTelefonos
+{
+    internal class Program
+    {
+        public static char[] Nums = ['0','1','2','3','4','5','6','7','8','9'];
+        public static char[] Guion = ['-'];
+        public static char[] Espacio = [' '];
+        public static char[] ParIzq = ['('];
+        public static char[] ParDer = [')'];
+        public static Dictionary<(string, char), string> transitions = new()
+        {
+            {("q0", 'n'), "q1"}, {("q0", 'r'), "q11"},
+            {("q1", 'n'), "q2"},
+            {("q3") }
+        };
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
